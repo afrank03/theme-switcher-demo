76fc8a4e16879959d166d4a32c2f546b9a70cb34
Arthur Frank <frank.artur0303@gmail.com>
2019-08-15 19:51:53 +0100

    [FEATURE] Adding login form to the app

---
+++ src/components/LoginForm.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="hello">
+    <h1>Login</h1>
+    <div class="c-login">
+      <div class="c-login__form">
+        <form>
+          <span class="c-app-name">Foo App</span>
+          <input type="text" placeholder="Login" />
+          <input type="text" placeholder="Password" />
+          <input type="button" value="Sumbit" />
+        </form>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'LoginForm',
+  data() {
+    return {};
+  },
+  methods: {},
+};
+</script>
+
+<style scoped lang="scss">
+.c-login {
+  display: flex;
+  justify-content: center;
+
+  &__form {
+    form {
+      display: flex;
+      align-items: center;
+      padding: 5px;
+      flex-direction: column;
+      border: 1px solid #ddd;
+      min-width: 300px;
+
+      .c-app-name {
+        align-self: flex-end;
+      }
+
+      input {
+        margin: 10px;
+        padding: 10px;
+        max-width: 200px;
+
+        &[type=button] {
+           border: 1px solid #aaa;
+           background: #fdfdfd;
+
+           &:hover {
+             background: #ddd;
+             cursor: pointer;
+           }
+        }
+      }
+    }
+  }
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,17 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Theme Swithcher app"/>
+    <LoginForm />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
+import LoginForm from '@/components/LoginForm.vue';
 
 export default {
   name: 'home',
   components: {
-    HelloWorld,
+    LoginForm,
   },
 };
 </script>
