324b798c59cb49b179c77d4cd196683dd7896684
Arthur Frank <Arthur.Franks@gocompare.com>
2019-08-30 09:54:42 +0100

    Added logout option. Check if user is logged in, if not redirect to login

--- src/views/UserProfile.vue
+++ src/views/UserProfile.vue
@@ -2,6 +2,7 @@
   <div class="c-user-profile">
     <h1>This is user profile page.</h1>
     <p><strong>Welcome</strong>: {{ currentUser.details.fullName }}</p>
+    <router-link to="/" @click="logout()">Logout</router-link>
   </div>
 </template>
 
@@ -14,9 +15,19 @@ export default {
       user: null
     }
   },
+  methods: {
+    logout() {
+      this.$store.dispatch('setUser', null);
+    }
+  },
   computed: {
     currentUser() {
       return this.$store.getters.currentUser;
+    }
+  },
+  created() {
+    if (this.currentUser === null) {
+      this.$router.push('/');
     }
   }
 };
